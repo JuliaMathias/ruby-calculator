5ca3e3a6ae7b06f4149ba6d5acea3d26fe7b6d8e
Julia Mathias <juliamss@gmail.com>
2021-01-23 16:41:49 -0300

    Added shopping list and empty list

---
+++ data.rb
@@ -0,0 +1,22 @@
+shopping_list = [{
+  item: "notebook",
+  quantity: rand(1..10);
+  price_per_unit: rand(850..1500);
+}, {
+  item: "pen",
+  quantity: rand(1..10);
+  price_per_unit: rand(150..1500);
+},
+{
+  item: "pencil",
+  quantity: rand(1..10);
+  price_per_unit: rand(150..1500);
+},{
+  item: "coloring utensil",
+  quantity: rand(1..10);
+  price_per_unit: rand(850..1500);
+}
+]
+
+empty_list = []
+
