a83a88671f8661b0a41a5eabc67e1624fd70f7bf
Julia Mathias <juliamss@gmail.com>
2021-01-23 18:46:47 -0300

    Added method to split the bill

--- calculator.rb
+++ calculator.rb
@@ -20,19 +20,49 @@ shopping_list = [{
 }
 ]
 
+people_list = [{
+  name: "Julia Mathias",
+  email: "juliamathias@gmail.com"
+}, {
+  name: "Renan Mathias",
+  email: "renanmathias@gmail.com"
+}, {
+  name: "Lais Mathias",
+  email: "laismathias@gmail.com"
+}]
+
 
 def add_values(list)
   total_cost = 0
-  puts "My total is #{total_cost}."
+  # puts "My total is #{total_cost}."
   list.each do |item|
-    puts "I will buy #{item[:quantity]} #{item[:item]}s, and each costs #{item[:price_per_unit]}."
+    # puts "I will buy #{item[:quantity]} #{item[:item]}s, and each costs #{item[:price_per_unit]}."
     cost_per_item = item[:price_per_unit] * item[:quantity]
-    puts "Buying #{item[:quantity]} #{item[:item]}s, costs #{cost_per_item}."
+    # puts "Buying #{item[:quantity]} #{item[:item]}s, costs #{cost_per_item}."
     total_cost += cost_per_item
-    puts "My total is now #{total_cost}."
+    # puts "My total is now #{total_cost}."
   end
-  puts total_cost
+  total_cost
 end
 
-add_values(shopping_list)
+
+
+def calculator (shopping_list, people_list)
+  total_cost = add_values(shopping_list)
+  number_of_people = people_list.length
+  base = Array.new(number_of_people, (total_cost / number_of_people))
+  remainder = total_cost % number_of_people
+  return base if remainder.zero?
+  while remainder > 0
+    base.each_with_index do |num, idx|
+      if remainder > 0
+        base[idx] += 1
+        remainder -= 1
+      end
+    end
+  end
+  puts base
+end
+
+calculator(shopping_list, people_list)
 
